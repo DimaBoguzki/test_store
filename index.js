f0c7ef6267ea9e3418b736992474f156e57a5dc7
Dima Boguzki <dimaboguzki@gmail.com>
2023-06-30 12:42:26 +0300

    prod

--- index.js
+++ index.js
@@ -2,29 +2,40 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const init = require('./server/init');
-// const path = require('path');
+const path = require('path');
 
 // init app db 
 init();
 
-
 const app = express();
 
-// Body Parser
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json());
-app.use(
-  cors({
-    origin: 'http://localhost:3000', 
-    methods: ['GET', 'POST', 'DELETE', 'PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  })
-);
-// Set static folder 
-// app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.render('index', res.send('Dima')));
+const isProd=true;
+
+if(!isProd){
+  app.use(
+    cors({
+      origin: 'http://localhost:3000', 
+      methods: ['GET', 'POST', 'DELETE', 'PATCH'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+    })
+  );
+}
+
+if(isProd){
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+
+}
+else{
+  app.get('/', (req, res) => res.render('index', res.send('DEV')));
+}
 
 app.use('/user', require('./server/routes/user'));
 app.use('/products', require('./server/routes/products'));
